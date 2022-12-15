3ecf21e56a0b764330b075f8b084cafed6ff96c8
Rostyslav Haluzinskiy <rossgaluzinskiy@gmail.com>
2022-12-15 14:25:26 +0200

    qwe

---
+++ src/js/fetch-service/api-vars.js
@@ -0,0 +1,7 @@
+const API_KEY = 'f896ccabafb5d2939071b9f1aa9e42c1';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TREND_URL = `${BASE_URL}/trending/movie/week`;
+const SEARCH_URL = `${BASE_URL}/search/movie`;
+const ID_URL = `${BASE_URL}/movie/`;
+
+export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL };

7d3343f7d6e65acae6ad0a96fc189e30891d46fb
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-12-17 01:01:06 +0200

    удалил ненужную разметку в main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,1 @@
 import FetchFilmsApi from './fetch-service/fechFilmsApi';
-
-// --------------------------------------------
-import { onCreat } from './main/renderMainMarkup';
-console.log(1);
-onCreat();

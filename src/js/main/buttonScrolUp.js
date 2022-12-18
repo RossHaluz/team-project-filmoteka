3e322a45c2b81015c5b31ffa3bfe80f3c0ba76eb
SlobodianiukDS <107937529+SlobodianiukDS@users.noreply.github.com>
2022-12-18 09:59:45 +0200

    создание кнопки вверх без стилей и анимации

---
+++ src/js/main/buttonScrolUp.js
@@ -0,0 +1,23 @@
+export function onButtonScrolUp() {
+  document.addEventListener('DOMContentLoaded', () => {
+    let to_top_btn = document.querySelector('.btn-goup');
+
+    console.log(`btn on`);
+
+    window.onscroll = function () {
+      if (window.pageYOffset > 580) {
+        to_top_btn.classList.remove('is-hidden');
+      } else {
+        to_top_btn.classList.add('is-hidden');
+      }
+    };
+
+    // плавный скролл наверх
+    to_top_btn.addEventListener('click', function () {
+      window.scrollBy({
+        top: 0,
+        behavior: 'smooth',
+      });
+    });
+  });
+}

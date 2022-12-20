ae9d510aaa32110c17189c7ed46829d19d3cf202
Rostyslav <rossgaluzinskiy@gmail.com>
2022-12-20 11:00:20 +0200

    add change color

--- src/js/fetch-service/refs.js
+++ src/js/fetch-service/refs.js
@@ -1,6 +1,7 @@
 const refs = {
     form: document.querySelector('.search-form__bar'),
-    formNotification: document.querySelector('.search-form__notification')
+    formNotification: document.querySelector('.search-form__notification'),
+    changeColor: document.querySelector('.changeColor'),
 }
 
 export { refs }
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@ import FetchFilmsApi from "./fetch-service/fechFilmsApi";
 import { onKeywordSearch } from "./main/searchMovies";
 import { onCreat, creatCards } from './main/renderMainMarkup';
 import { onButtonScrolUp } from './main/buttonScrolUp'; 
+import { onClickChangeColor } from "./main/changeColor";
+
 onButtonScrolUp();
 onCreat()
 

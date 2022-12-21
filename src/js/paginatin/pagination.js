62a790865b0045a61e1af701654b9195c9aabe06
Rostyslav <rossgaluzinskiy@gmail.com>
2022-12-21 12:06:24 +0200

    add

--- src/js/paginatin/pagination.js
+++ src/js/paginatin/pagination.js
@@ -3,31 +3,20 @@ import 'tui-pagination/dist/tui-pagination.css';
 import FetchFilmsApi from '../fetch-service/fechFilmsApi';
 import { refs } from '../fetch-service/refs';
 
-export function createPagination() {
+export function createPagination(totalItems, visiblePages) {
   const options = {
-  totalItems: 500,
- itemsPerPage: 10,
- visiblePages: 5,
-  centerAlign: true,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
-};
+    itemsPerPage: 20,
+    totalItems: totalItems,
+    visiblePages: visiblePages < 5 ? visiblePages : TUI_VISIBLE_PAGES,
+  };
 
-const pagination = new Pagination('pagination', options);
+  const pagination = new Pagination(refs.pagination, options);
+
+  if (visiblePages > 1) {
+    refs.pagination.style.display = 'block';
+  } else {
+    refs.pagination.style.display = 'none';
+  }
+
+  return pagination;
 }

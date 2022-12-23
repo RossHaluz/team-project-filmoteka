529808c03bc97e4a944bf8f2fc0fd6afc442ade1
Natalia <natalakrakoveckaya@gmail.com>
2022-12-23 16:46:56 +0100

    Summary

--- src/js/main-library.js
+++ src/js/main-library.js
@@ -1,7 +1,6 @@
-import refs from "./fetch-service/refs";
-import { LocalStorageServiceFilms } from "./fetch-service/localStorageService";
-import { addWatched } from "./main/renderLibrary";
-import { onClickBtnQueue, onClickBtnWatched } from "./main/header-library-btn";
-import { getFilmsById } from "./main/renderLibraryMarkup";
-getFilmsById()
-
+import refs from './fetch-service/refs';
+import { LocalStorageServiceFilms } from './fetch-service/localStorageService';
+import { addWatched } from './main/renderLibrary';
+import { onClickBtnQueue, onClickBtnWatched } from './main/header-library-btn';
+import { getFilmsById } from './main/renderLibraryMarkup';
+getFilmsById();
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
-import FetchFilmsApi from "./fetch-service/fechFilmsApi";
-import { onKeywordSearch } from "./main/searchMovies";
+import FetchFilmsApi from './fetch-service/fechFilmsApi';
+import { onKeywordSearch } from './main/searchMovies';
 import { onCreat, creatCards } from './main/renderMainMarkup';
 import { onButtonScrolUp } from './main/buttonScrolUp';
 import { onClickChangeColor } from './main/changeColor';
 import { createMarkup } from './fetch-service/renderFuncApi';
 import { openModal } from './modal/modals';
+import { openTeamModal } from './modal/close-modal';
 onButtonScrolUp();
-onCreat()
+onCreat();
 // addListenerToCard();
-

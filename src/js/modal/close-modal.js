86ff827ddf1ad3145d58e2c8c3d01c0926cc0860
andriideikalo <wlada801@gmail.com>
2022-12-24 11:55:13 +0200

    fix

--- src/js/modal/close-modal.js
+++ src/js/modal/close-modal.js
@@ -8,27 +8,25 @@ refs.btnCloseModal.addEventListener('click', toggleModal);
 
 function toggleModal(evt) {
     evt.preventDefault();
+    document.body.classList.add('no-scroll');
     refs.teamModal.classList.toggle('is-hidden');
 
 }
-window.addEventListener('keydown', onEscClick)
+window.addEventListener('keydown', onEscClick);
 
 function onEscClick(evt) {
     if (evt.code === 'Escape') {
-        // console.log('Escape')
         evt.preventDefault();
         refs.teamModal.classList.add('is-hidden');
     }
 }
 
-const backdrop = document.querySelector('.footer__backdrop')
-backdrop.addEventListener('click', onBackdropClick)
-console.log(backdrop)
-
+const backdrop = document.querySelector('.footer__backdrop');
+backdrop.addEventListener('click', onBackdropClick);
+console.log(backdrop);
 
 function onBackdropClick(evt) {
     if (evt.currentTarget === evt.target) {
-        // console.log('Backdrop')
         evt.preventDefault();
         refs.teamModal.classList.add('is-hidden');
     }

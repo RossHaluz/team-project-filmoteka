3488f92ce3e14c8ec6718027a5a65ea26103a6d6
Rostyslav <rossgaluzinskiy@gmail.com>
2022-12-24 19:36:09 +0200

    fix. pagination

--- src/js/paginatin/pagination.js
+++ src/js/paginatin/pagination.js
@@ -1,8 +1,10 @@
 import FetchFilmsApi from "../fetch-service/fechFilmsApi";
 import { refs } from "../fetch-service/refs";
+import { onCreat, creatCards, genresSerch, checkYear } from '../main/renderMainMarkup';
 
 const fetchApi = new FetchFilmsApi();
 let currentPage = 1;
+let allGenres = {}; 
 const options = { mediaType: 'movie', timeWindow: 'week' }
 
 refs.paginationListItemPrev.addEventListener('click', onClickBtnPrev);
@@ -13,7 +15,7 @@ export function onClickBtnNext() {
   currentPage += 1;
   fetchApi.actualPage = currentPage;
   refs.paginationListItemPage.innerHTML = currentPage;
-  fetchApi.fetchWithAllFilmsData(options).then(data => creatMarkupPagination(data.data.results))
+  fetchApi.fetchWithAllFilmsData(options).then(data => creatCards(data.data.results))
 }
 
 function onClickBtnPrev() {
@@ -24,26 +26,5 @@ function onClickBtnPrev() {
   currentPage -= 1;
   fetchApi.actualPage = currentPage;
   refs.paginationListItemPage.innerHTML = currentPage;
-  fetchApi.fetchWithAllFilmsData(options).then(data => creatMarkupPagination(data.data.results))
-}
-
-function creatMarkupPagination(arr) {
-  const markup = arr.map(({ poster_path, title, release_date, id }) => `
-  <li class="galery-list__item card-set-iteam">
-          <img data-id=${id} src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}" class="galery-list__img" />
-          <h3 class="galery-list__title">${title}</h3>
-          <p class="galery-list__desc">
-            Science Fiction, Action, Adventure, Drama | ${splitDate(release_date)}
-          </p>
-        </li>
-  `).join('');
-
-  refs.gallery.innerHTML = markup;
-}
-
-function splitDate(value) {
-    const slit = value.split('').slice(0, 4).join('');
-
-    return slit;
-
+  fetchApi.fetchWithAllFilmsData(options).then(data => creatCards(data.data.results))
 }

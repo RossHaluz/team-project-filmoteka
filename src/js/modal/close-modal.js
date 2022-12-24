e93664ee55c0fab5601b2fcd893f44cd3ead92f1
andriideikalo <wlada801@gmail.com>
2022-12-24 13:06:05 +0200

    fix

--- src/js/modal/close-modal.js
+++ src/js/modal/close-modal.js
@@ -7,18 +7,18 @@ refs.linkOpenModal.addEventListener('click', toggleModal);
 refs.btnCloseModal.addEventListener('click', toggleModal);
 
 function toggleModal(evt) {
-    evt.preventDefault();
-    document.body.classList.add('no-scroll');
-    refs.teamModal.classList.toggle('is-hidden');
+  evt.preventDefault();
+  document.body.classList.add('no-scroll');
+  refs.teamModal.classList.toggle('is-hidden');
 }
 window.addEventListener('keydown', onEscClick);
 
 function onEscClick(evt) {
-    if (evt.code === 'Escape') {
-        console.log('Escape');
-        evt.preventDefault();
-        refs.teamModal.classList.add('is-hidden');
-    }
+  if (evt.code === 'Escape') {
+    console.log('Escape');
+    evt.preventDefault();
+    refs.teamModal.classList.add('is-hidden');
+  }
 }
 
 const backdrop = document.querySelector('.footer__backdrop');
@@ -26,9 +26,9 @@ backdrop.addEventListener('click', onBackdropClick);
 console.log(backdrop);
 
 function onBackdropClick(evt) {
-    if (evt.currentTarget === evt.target) {
-        console.log('Backdrop');
-        evt.preventDefault();
-        refs.teamModal.classList.add('is-hidden');
-    }
+  if (evt.currentTarget === evt.target) {
+    console.log('Backdrop');
+    evt.preventDefault();
+    refs.teamModal.classList.add('is-hidden');
+  }
 }
